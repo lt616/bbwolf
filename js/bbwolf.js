ea9e05ae563b20f292106bb94d7bd7453af7106b
lt616 <CherryZHAO@axjllt.local>
2018-01-23 05:22:00 +1100

    project info build

---
+++ js/bbwolf.js
@@ -0,0 +1,8 @@
+function display(id_name) {
+	document.getElementById(id_name).style.display = 'block'; 
+} 
+
+function undisplay(id_name) {
+	document.getElementById(id_name).style.display = "none"; 
+}
+
